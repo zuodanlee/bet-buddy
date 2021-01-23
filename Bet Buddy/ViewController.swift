5564fc4d4c0a1b5347a5bcc7297365cb3112788f
Jordan Lee <s10196128@connect.np.edu.sg>
2021-01-23 16:17:22 +0800

    Initial Commit

---
+++ Bet Buddy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bet Buddy
+//
+//  Created by Jordan Lee on 23/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
